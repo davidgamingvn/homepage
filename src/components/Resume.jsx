2e451fcfc79a672cb0d6c544c762a525a0969df9
davidgamingvn <davidarthur260702@gmail.com>
2024-09-02 13:15:29 -0700

    added dynamic resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,22 +1,29 @@
-import React, { useState } from "react";
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import { getFilePlugin } from "@react-pdf-viewer/get-file";
-import resume from "../assets/resume.pdf";
+import React, { useState, useEffect } from "react";
 import { Flex, Button, Box } from "@chakra-ui/react";
-import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { ViewIcon, ViewOffIcon, DownloadIcon } from "@chakra-ui/icons";
 import { useColorMode } from "@chakra-ui/color-mode";
+import getResumeFromOneDrive from "../utils/fetchOneDrive";
 
 const Resume = () => {
-  const getFilePluginInstance = getFilePlugin();
-  const { DownloadButton } = getFilePluginInstance;
   const [showPdf, setShowPdf] = useState(false);
+  const [resumeUrl, setResumeUrl] = useState(null);
+  const [downloadUrl, setDownloadUrl] = useState(null)
   const { colorMode } = useColorMode();
 
   const bgColor = {
     light: "transparent", // background color when light color mode
     dark: "white", // background color when dark color mode
   };
+
+  useEffect(() => {
+    const fetchResume = async () => {
+      const [resumeUrl, downloadUrl] = await getResumeFromOneDrive();
+      setResumeUrl(resumeUrl);
+      setDownloadUrl(downloadUrl);
+    };
+
+    fetchResume();
+  }, []);
 
   return (
     <div>
@@ -28,22 +35,29 @@ const Resume = () => {
           Resume
         </Button>
       </Flex>
-      {showPdf && (
+      {showPdf && resumeUrl && (
         <Flex
-          direction={{ base: "column", md: "row", lg: "row"}}
+          direction={{ base: "column", md: "row", lg: "row" }}
           justifyContent="center"
           alignItems="center"
           height="600px"
           width={{ base: "90%", md: "75%", lg: "50%" }}
           margin="auto"
+          gap={5}
           padding={5}
         >
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-            <Box bg={bgColor[colorMode]} color="white" padding={1} borderRadius="md" margin="2">
-              <DownloadButton />
-            </Box>
-            <Viewer fileUrl={resume} plugins={[getFilePluginInstance]} />
-          </Worker>
+          <Button variant="ghost">
+            <a href={downloadUrl} download="Phuc_Nguyen_Resume.pdf">
+              <DownloadIcon />
+            </a>
+          </Button>
+          <iframe
+            src={resumeUrl}
+            width="100%"
+            height="100%"
+            allowfullscreen
+            title="Phuc_Nguyen_Resume"
+          ></iframe>
         </Flex>
       )}
     </div>
